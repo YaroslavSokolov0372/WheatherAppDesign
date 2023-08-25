01fd196cda5b2308597d3ca26912b59ca8847611
YaroslavSokolov0372 <yaroslav.sokolov0372@gmail.com>
2023-08-25 04:45:46 +0200

    Initial Commit

---
+++ WheatherApp/WheatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheatherAppApp.swift
+//  WheatherApp
+//
+//  Created by Yaroslav Sokolov on 25/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct WheatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
